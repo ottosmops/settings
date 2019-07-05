4d1a57fe0851942c7ebe53e42607d036211e632e
andreas_kraenzle <kraenzle@k-r.ch>
2019-07-05 10:41:42 +0200

    fix ServiceProvider

--- src/SettingsServiceProvider.php
+++ src/SettingsServiceProvider.php
@@ -9,9 +9,14 @@ class SettingsServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__ . '/../database/migrations/' => base_path('/database/migrations'),
-            __DIR__.'/../config/settings.php' => config_path('settings.php'),
-        ]);
+            __DIR__ . '/../config/settings.php' => config_path('settings.php'),
+        ], 'config');
+
+        if (! class_exists('CreateSettingsTable')) {
+            $this->publishes([
+                    __DIR__ . '/../database/migrations/create_settings_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()). '_create_settings_table.php')
+            ], 'migrations');
+        }
     }
 
     public function register()
