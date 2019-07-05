8fe5a8770bd1408e548fc55f5edfc12bf0493db4
andreas_kraenzle <kraenzle@k-r.ch>
2019-07-05 11:46:33 +0200

    fix service provider

--- database/migrations/create_settings_table.php
+++ database/migrations/create_settings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class SettingsTable extends Migration
+class CreateSettingsTable extends Migration
 {
     protected $table;
 
